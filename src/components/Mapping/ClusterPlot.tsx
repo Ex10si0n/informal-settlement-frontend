b4b50f8a581cb9ab017383261fb87f7294437ca2
ex10si0n <yan.ex10si0n@icloud.com>
2024-08-03 21:42:23 -0400

    [v0.1] mapping update: TODO: display cluster plot

--- src/components/Mapping/ClusterPlot.tsx
+++ src/components/Mapping/ClusterPlot.tsx
@@ -12,6 +12,7 @@ const MappingClusterPlot: React.FC<ClusterPlot> = () => {
           <Accordion.Title className="p-3 focus:ring-0">View Clustering Plot</Accordion.Title>
           <Accordion.Content>
             <div className="flex items-center justify-center rounded-b-lg h-64 bg-gray-50 dark:bg-gray-800">
+              {/*TODO: display cluster plot*/}
               Cluster Plot
             </div>
           </Accordion.Content>
